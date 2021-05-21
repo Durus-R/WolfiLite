faed0eda4beb69cae5fbfe5cdf59d87695cbd9ff
Ronnie1320 <68396806+Ronnie1320@users.noreply.github.com>
2021-05-21 12:23:54 +0200

    Create setup.bat

---
+++ setup.bat
@@ -0,0 +1,5 @@
+SET Genius="Your Genius Token"
+SET Reddit_ID="Your Reddit_ID"
+SET Reddit_Secret="Your Reddit Secret"
+SET WolframAlpha="Your Wolfam|Alpha APP_ID"
+SET token="Your Discord bot token"
