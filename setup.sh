3da440cb3664856c126be51b2bae2c98c0fc6fff
Ronnie1320 <68396806+Ronnie1320@users.noreply.github.com>
2021-05-21 12:21:36 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+# !/bin/bash
+export Genius="Your genius token"
+export Reddit_ID="Your Reddit ID"
+export Reddit_Secret="Your Reddit Secret"
+export WolframAlpha="Your Wolfram|Alpha App_ID"
+export token="Your Bot token"
